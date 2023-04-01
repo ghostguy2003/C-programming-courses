8121b741668124cb9bd075886abde24394db1e64
ghostguy2003 <125500957+ghostguy2003@users.noreply.github.com>
2023-04-01 22:10:34 +0200

    Create char.c
    prints programming in c by format specifier

---
+++ projects/char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+    char letter1,letter2,lag;
+    letter1='i';
+    letter2 = 'n';
+    lag='C';
+    printf("programming %c%c %c",letter1,letter2,lag);
+    return 0;
+}
