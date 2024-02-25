cdc9a57e1688a1d54c078bbdb02a796ac3fca7f3
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:47:04 +0200

    Update 3-big cities.c

--- 3-big cities.c
+++ 3-big cities.c
@@ -1,3 +1,6 @@
+/*You want to determine the number of cities in a given region that have a population strictly greater than 10,000. 
+To do this, you write a program that first reads the number of cities in a region as an integer, and then the populations for each city one by one (also integers).*/
+
 #include <stdio.h>
 
 int main (void){
