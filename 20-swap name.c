b81d796edc54bccd4e0ec096264dc250a1e4ce25
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:39:38 +0200

    Update 20-swap name.c

--- 20-swap name.c
+++ 20-swap name.c
@@ -1,3 +1,8 @@
+/*Your program should first read the total number of names (an integer) in order to know how many index cards need to be processed altogether.
+Next,  for each index card, your program should read a patron's first name and last name and then display these names correctly, that is on one line, the last name followed by one space, followed by the first name. 
+Your program should print the reversed name immediately after reading the patron's names (ie, it should not wait until it has read all of the index cards to begin printing).*/
+
+
 
 int main (void){
     int input,i;
