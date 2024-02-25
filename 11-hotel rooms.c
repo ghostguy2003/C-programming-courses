e62ef3c120aad34e59cbc1b3142373edeafefd87
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:22:48 +0200

    Update 11-hotel rooms.c

--- 11-hotel rooms.c
+++ 11-hotel rooms.c
@@ -1,3 +1,8 @@
+/*One room costs nothing if you are exactly 60 (the age of the innkeeper), or 5 dollars if you are less than 10 years old. 
+For everyone else, the cost is 30 dollars plus an additional 10 dollars if you bring more than 20 pounds of luggage. 
+Your program should read the customer's age first, then the weight of their luggage, then output the price they have to pay.*/
+
+
 #include <stdio.h>
 
 int main (void){
