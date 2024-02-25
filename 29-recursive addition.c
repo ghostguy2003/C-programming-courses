08ec22057bd91ca0dbfdc5e71ac7d1edc5677efb
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:17:49 +0200

    Update 29-recursive addition.c

--- 29-recursive addition.c
+++ 29-recursive addition.c
@@ -1,3 +1,8 @@
+/*Write a C-program that uses a recursive function called "sumOfDigits()" to compute the sum of the digits of a number. 
+To do so, your main function should first read an integer number as input and then call sumOfDigits(), which should in turn call itself until there are no digits left to sum, at which point the final sum should display to the user.*/
+
+
+
 int sumOfDigits(int);
 
 #include <stdio.h>
