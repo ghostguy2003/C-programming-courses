529237224afb99aa6319be0e158b3e90b64e80dc
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:21:36 +0200

    Update 10-guess my number.c

--- 10-guess my number.c
+++ 10-guess my number.c
@@ -1,3 +1,8 @@
+/*Your program must first read an integer indicating the number that the child will have to find during the game.
+Next the program should repeatedly read the player's guesses and display the text "it is more" if the child has submitted a smaller number or "it is less" if they have submitted a larger number.
+Once the correct number is reached, the program should print "Number of tries needed:" followed by a new line and the integer number of tries that it took the guesser.*/
+
+
 #include <stdio.h>
 
 int main (void){
