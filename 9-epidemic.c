59cdf0b2fe413c200d98b9c9f2e8b038ee80b870
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 21:13:49 +0200

    Update 9-epidemic.c

--- 9-epidemic.c
+++ 9-epidemic.c
@@ -1,3 +1,9 @@
+/*For a total population of 100 inhabitants, on day 1 a single person is infected. 
+This is followed by 2 new people on the second day for a total of 3 infected people. On the third day,  6 new people are infected for a total of 9 infected people.
+On the fourth day, 18 new people are infected, for a total of 27 people. On the fifth day, 27 new people infect 54 people for a total of 81 people infected.
+On the sixth day, the last of the 100 people will be infected (though the 81 people have the potential to contaminate 162 people) so your program should output '6'.*/
+
+
 #include <stdio.h>
 
 int main (void){
