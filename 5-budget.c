96379c585c060821b5fea47b6e7e4c0548975632
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:49:35 +0200

    Update 5-budget.c

--- 5-budget.c
+++ 5-budget.c
@@ -1,3 +1,7 @@
+/*Your program will have to read a sequence of positive integers and display their sum.
+We do not know how many integers there will be, but the sequence always ends with the value -1 (which is not an expense, just an end marker).*/
+
+   
 #include <stdio.h>
 
 int main (void){
