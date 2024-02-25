e0b45d08ac8d6687282252e27676129724a09bf7
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-02-25 20:28:03 +0200

    Update 13-longest word in text.c

--- 13-longest word in text.c
+++ 13-longest word in text.c
@@ -1,3 +1,8 @@
+/*Your job is to find the length of the longest word in a text with no punctuation or special characters of any kind - only contains words. 
+To do so, please write a C-program that takes as a input first the number of words in a text, followed by all of the words in the text.
+The output of your program should be the length of the longest word in the text.*/
+
+
 #include <stdio.h>
 
 int main (void){
